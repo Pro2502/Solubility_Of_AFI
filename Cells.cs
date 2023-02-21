602d0e4119502230e2173f14f007439428edbacd
Anna <anna.propisnova@list.ru>
2023-02-21 15:00:31 +0300

    Начальный коммит с прошлого проекта

---
+++ Cells.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Realization
+{
+    class Cells
+    {
+        public int saturated_solution = 250;
+        public float x { get; set; }
+        public float y { get; set; }
+
+        public double concentration;
+        public bool IsSolid { get; private set; }
+        public bool Luquid { get; private set; }
+
+        private void Switch_Solid()
+        {
+            if (concentration < saturated_solution)
+            {
+                IsSolid = false;
+            }
+        }
+        public int Concentration
+        {
+            set
+            {
+                concentration = value;
+            }
+            get
+            {
+                int new_C = Convert.ToInt32(concentration);
+                return new_C;
+            }
+        }
+        public bool CalcSolid()
+        {
+            return concentration >= saturated_solution;
+
+        }
+        public bool CalcLuquid()
+        {
+            return concentration < saturated_solution;
+
+        }
+    }
+}
---
+++ Tablets.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Realization
+{
+    class Tablets
+    {
+        Random rnd = new Random();
+        public int NumberOfTablets = 1;
+        public double R;
+        //int TabletArea;
+        public Tablets(int length, int width,double D)
+        {
+            //int FieldArea = length * width ;
+            //int percentLuquid = 90-rnd.Next(30);
+            //int percentForTablets = 100 - percentLuquid;
+            //double percent = percentForTablets / 100;
+            //double numbOfCellsForTablets = FieldArea * percent;
+            //int totalTablets = (int)numbOfCellsForTablets;
+            R = D / 2;
+            //TabletArea = (int)(((int)Math.Pow(R,2)) * Math.PI);
+            //NumberOfTablets = totalTablets / TabletArea;
+        }
+    }
+}
